a52b2e61ff9648aba9a8ab8f5baf402221ddb60a
Helenilson Matias <Helenilson.soares@tcs.com>
2018-10-08 13:23:09 -0500

    primeiro commit microservice

---
+++ src/main/java/com/hms/microservice/LimiteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hms.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimiteServiceApplication.class, args);
+	}
+}
